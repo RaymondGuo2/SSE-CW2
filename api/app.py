fe44b2b121cf3916af4fc110ab980ca095804c97
Sach Nehal <sn2023@ic.ac.uk>
2023-10-30 15:08:31 +0000

    what is your name

--- api/app.py
+++ api/app.py
@@ -19,6 +19,8 @@ def submit():
 def process_query(query):
     if query == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
+    if query == "What is your name?":
+        return "SSE_LEGENDS"
     else:
         return "Unknown"
 
