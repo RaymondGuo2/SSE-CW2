bd254b2a949787f235009e140674895dd56dd053
Sach Nehal <sn2023@ic.ac.uk>
2023-10-30 15:09:48 +0000

    what is your name

--- api/app_test.py
+++ api/app_test.py
@@ -12,4 +12,4 @@ def test_does_not_know_about_asteroids():
 
 
 def test_what_is_your_name():
-    assert process_query("What is your name?" == "SSE_LEGENDS"
+    assert process_query("What is your name?") == "SSE_LEGENDS"
