e42b135fd3c4bd127c918658be859e31527a0d22
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-10-30 15:28:10 +0000

    First batch

--- api/app_test.py
+++ api/app_test.py
@@ -13,3 +13,12 @@ def test_does_not_know_about_asteroids():
 
 def test_what_is_your_name():
     assert process_query("What is your name?") == "SSE_LEGENDS"
+
+def first_addition():
+    assert process_query("What is 71 plus 61") == "132"
+
+def largest_number1():
+    assert process_query("Which of the following numbers is the largest: 87, 13, 84?") == "87"
+
+def largest_number2():
+    assert process_query("Which of the following numbers is the largest: 11, 78, 54?") == "78"
