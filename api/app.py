a6c71353431a0e6eb148c02a6f193a322e017eab
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-11-06 21:23:21 +0000

    Part 3

--- api/app.py
+++ api/app.py
@@ -110,6 +110,6 @@ def returngithub():
     if response.status_code == 200:
         repos = response.json()
         return jsonify([repo["full_name"] for repo in repos])
-    
+
     input_username = request.form.get("username")
     return render_template("returngitname.html", username=input_username)
