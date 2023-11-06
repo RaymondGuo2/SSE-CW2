ac9e984f606198a6c4de7058dba1a304251828d1
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-11-06 16:16:09 +0000

    Infrastructure

--- api/app.py
+++ api/app.py
@@ -99,7 +99,7 @@ def githubuname():
     return render_template("githubuname.html")
 
 
-@app.route("/returngitname", methods=["POST"])
-def returngitname():
+@app.route("/returngitname", methods=["GET", "POST"])
+def returngithub():
     input_username = request.form.get("username")
     return render_template("returngitname.html", name=input_username)
