fce26bd5c582381aa1ced4157b471ecdeadd259b
Sach Nehal <sn2023@ic.ac.uk>
2023-11-09 01:18:21 +0000

    get_commit dates + counts

--- api/app.py
+++ api/app.py
@@ -154,6 +154,8 @@ def get_commit_counts(owner, repo):
 def returngithub():
     input_username = request.form.get("username")
     repos = []
+    commit_counts = []
+    commit_dates = []
     error_message = None
 
     try:
@@ -169,8 +171,8 @@ def returngithub():
             commits_response.raise_for_status()
 
             repo_name = repo["name"]
-            _commit_counts = get_commit_counts(input_username, repo_name)
-            _commit_dates = get_commit_dates(input_username, repo_name)
+            commit_counts = get_commit_counts(input_username, repo_name)
+            commit_dates = get_commit_dates(input_username, repo_name)
 
             commits = commits_response.json()
             latest_commit = commits[0] if commits else None
@@ -181,8 +183,6 @@ def returngithub():
                 "language": repo["language"],
                 "created_at": repo["created_at"],
                 "updated_at": repo["updated_at"],
-                "commit_counts": _commit_counts,
-                "commit_dates": _commit_dates,
                 "latest_commit": {
                     "hash": (
                         latest_commit["sha"]
@@ -220,5 +220,9 @@ def returngithub():
         return render_template("error.html", error_message=error_message), 500
 
     return render_template(
-        "returngitname.html", username=input_username, repos=repos
+        "returngitname.html",
+        username=input_username,
+        repos=repos,
+        commit_counts=commit_counts,
+        commit_dates=commit_dates
     )
