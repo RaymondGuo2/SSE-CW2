e1da3d83f6dd977798ab3fc4ecc695471f3b0c8d
Sach Nehal <sn2023@ic.ac.uk>
2023-11-27 15:51:13 +0000

    test db

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, pyscopg
+from flask import Flask, render_template, request
 import requests
-import psycopg as db
+from psycopg import psycopg as db
 import configparser
 import logging
 
