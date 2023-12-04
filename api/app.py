519bf2be66dee7c615f28cf3d25ad30e222f5421
Sach Nehal <sn2023@ic.ac.uk>
2023-12-04 22:21:26 +0000

    TRYING db setup separate

--- api/app.py
+++ api/app.py
@@ -83,79 +83,7 @@ def jumper_page():
     return render_template("jumper.html")
 
 
-def createTableItem():
-    DBNAME = os.environ.get('DBNAME')
-    HOST = os.environ.get('HOST')
-    PORT = os.environ.get('PORT')
-    USER = os.environ.get('USER')
-    PASSWORD = os.environ.get('PASSWORD')
-    CLIENT_ENCODING = os.environ.get('CLIENT_ENCODING')
-    server_params = {'dbname': DBNAME,
-                     'host': HOST,
-                     'port': PORT,
-                     'user': USER,
-                     'password': PASSWORD,
-                     'client_encoding': CLIENT_ENCODING}
-    conn = db.connect(**server_params)
-    curs = conn.cursor()
-    curs.execute("DROP TABLE items")
-    try:
-        curs.execute("""
-CREATE TABLE item (
-item_id SERIAL PRIMARY KEY,
-item_name VARCHAR(20) NOT NULL,
-price DECIMAL(10,2) NOT NULL,
-type VARCHAR(20) NOT NULL,
-stock INTEGER NOT NULL,
-color VARCHAR(20) NOT NULL,
-size VARCHAR(2) NOT NULL
-)
-""")
-        conn.commit()
-    except db.ProgrammingError:
-        conn.rollback()
-    finally:
-        conn.close()
-
-
-def insertItem(
-        name: str,
-        price: int,
-        itemType: str,
-        stock: int,
-        color: str,
-        size: str):
-    DBNAME = os.environ.get('DBNAME')
-    HOST = os.environ.get('HOST')
-    PORT = os.environ.get('PORT')
-    USER = os.environ.get('USER')
-    PASSWORD = os.environ.get('PASSWORD')
-    CLIENT_ENCODING = os.environ.get('CLIENT_ENCODING')
-    server_params = {'dbname': DBNAME,
-                     'host': HOST,
-                     'port': PORT,
-                     'user': USER,
-                     'password': PASSWORD,
-                     'client_encoding': CLIENT_ENCODING}
-    conn = db.connect(**server_params)
-    curs = conn.cursor()
-    curs.execute("DELETE FROM item")
-    curs.execute("""
-INSERT INTO item (item_name, price, type, stock, color, size)
-VALUES (%s, %s, %s, %s, %s, %s)
-""", (name, price, itemType, stock, color, size))
-    conn.commit()
-    conn.close()
-
-
-def setupDB():
-    createTableItem()
-    insertItem('Black Beanie', 12, 'Hat', 10, 'Black', 'M')
-    insertItem('Green Beanie', 12, 'Hat', 10, 'Green', 'M')
-    insertItem('Hugo Boss Jumper', 60, 'Jumper', 10, 'Grey', 'M')
-    insertItem('Uniqlo Jumper', 40, 'Jumper', 10, 'Green', 'M')
-    insertItem('Air Force 1s', 60, 'Shoe', 10, 'White', '11')
-    insertItem('Vans', 50, 'Shoe', 10, 'Black', '11')
+def connectDB():
     DBNAME = os.environ.get('DBNAME')
     HOST = os.environ.get('HOST')
     PORT = os.environ.get('PORT')
@@ -172,6 +100,10 @@ def setupDB():
     }
     conn = db.connect(**server_params)
     curs = conn.cursor()
+
+
+def dbQuery():
+    connectDB()
     curs.execute("SELECT* FROM item")
     response = curs.fetchall()
     conn.close()
@@ -180,7 +112,7 @@ def setupDB():
 
 @app.route("/database")
 def database_page():
-    responsesql = setupDB()
+    responsesql = dbQuery()
     return render_template("database.html", response=responsesql)
 
 
