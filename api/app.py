a3bb81e93e892f7bb1bb59523478f5c7e93f1b1e
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-12-07 10:55:30 +0000

    footer

--- api/app.py
+++ api/app.py
@@ -27,6 +27,11 @@ def checkout():
     return render_template("checkout.html")
 
 
+@app.route("/basket")
+def basket():
+    return render_template("basket.html")
+
+
 @app.route("/place_order", methods=["POST"])
 def place_order():
     input_name = request.form.get("name")
