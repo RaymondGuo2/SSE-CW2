f12497acdfcddefbc032667d147f02e10ddf4def
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-12-10 14:50:49 +0000

    Testing

--- api/app_test.py
+++ api/app_test.py
@@ -34,6 +34,31 @@ def basket_test(client):
     assert b"Adidas" in response.data
 
 
+def form_test(client):
+    response = client.post("/thankyou", data={
+        "name": "Alan Smith",
+        "email": "asmith@gmail.com",
+        "message": "Fantastic Service!"
+    })
+    assert response.status_code == 200
+    assert b"Expected response" in response.data
+
+
+def search_test(client):
+    search_query = "shoes"
+    response = client.get(f"/search?query={search_query}")
+    assert response.status_code == 200
+    assert search_query.encode() in response.data
+
+
+def currency_convert_test(client):
+    original_price = "10"
+    currency = "GBP"
+    response = client.get(f"/convert_currency?price={original_price}&currency={currency}")
+    assert response.status_code == 200
+    assert "Price converted" in response.json
+
+
 def test_knows_about_dinosaurs():
     assert process_query("dinosaurs") == (
         "Dinosaurs ruled the Earth 200 million years ago"
