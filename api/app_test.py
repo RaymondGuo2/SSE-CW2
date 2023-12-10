f2a15e830b3ad37bb7033a929e6fcb33f39d5c08
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-12-10 14:38:54 +0000

    Testing

--- api/app_test.py
+++ api/app_test.py
@@ -16,6 +16,24 @@ def home_page_test(client):
     assert b"Expected content on home page" in response.data
 
 
+def form_test(client):
+    response = client.post("/submit", data={
+        "name": "Alan Smith",
+        "email": "asmith@gmail.com",
+        "message": "Fantastic Service!"
+    })
+    assert response.status_code == 200
+    assert b"Expected response" in response.data
+
+
+def basket_test(client):
+    with client.session_transaction() as se:
+        se['cart'] = [{'itemName': 'Adidas', 'quantity': 2, 'price': 10}]
+    response = client.get("/basket")
+    assert response.status_code == 200
+    assert b"Adidas" in response.data
+
+
 def test_knows_about_dinosaurs():
     assert process_query("dinosaurs") == (
         "Dinosaurs ruled the Earth 200 million years ago"
