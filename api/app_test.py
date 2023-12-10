d1ebcba79d54e02ad654f58494ac8252e35b387c
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-12-10 14:23:40 +0000

    Testing

--- api/app_test.py
+++ api/app_test.py
@@ -1,7 +1,24 @@
+import pytest
+from app import app as flask_app
 from app import process_query
+
+
+def app():
+    yield flask_app
+
+
+def client(app):
+    return app.test_client()
+
+
+def home_page_test(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Expected content on home page" in response.data
 
 
 def test_knows_about_dinosaurs():
     assert process_query("dinosaurs") == (
         "Dinosaurs ruled the Earth 200 million years ago"
     )
+
