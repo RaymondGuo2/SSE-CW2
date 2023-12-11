c4b88d85bb656fa8677426db7cee784f6e9903af
Sach Nehal <sn2023@ic.ac.uk>
2023-12-11 12:58:09 +0000

    db try, except, finally

--- api/app.py
+++ api/app.py
@@ -166,7 +166,7 @@ def dbQuery():
     unformatted_response = curs.fetchall()
     response = [
         (
-         item[0],
+         item[0].strip("'"),
          item[1].strip("'"),
          f"{item[2]:.2f}",
          item[3].strip("'"),
